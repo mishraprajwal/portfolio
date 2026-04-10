c7be4615eba41a6221390c4f82b2eb55c6c1db7b
mishraprajwal <prajwalm882@gmail.com>
2026-04-09 19:02:50 -0700

    mobile res fix - career

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <div className="flex flex-1 justify-center max-sm:hidden">
+        <div className="flex flex-1 justify-center flex-wrap gap-1">
           {navLists.map((nav) => (
             <a
               key={nav}
